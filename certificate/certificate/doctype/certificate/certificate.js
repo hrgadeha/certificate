4d9391cbcd2264d5bde8ac3cef8d0f43930d3c26
Your Name <you@example.com>
2018-10-10 12:47:12 +0530

    added employee and company certificate document

---
+++ certificate/certificate/doctype/certificate/certificate.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, Hardik Gadesha and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Certificate', {
+	refresh: function(frm) {
+
+	}
+});
